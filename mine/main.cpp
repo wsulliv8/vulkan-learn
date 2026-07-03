47fe0981f29e8c4792bdbffa9d4128ef69fba5a0
Will Sullivan <wsulliv8@users.noreply.github.com>
2026-07-02 22:23:51 -0700

    Vulkan setup

--- mine/main.cpp
+++ mine/main.cpp
@@ -1,9 +1,29 @@
-#include <cstdint>
-#include <vulkan/vulkan.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_vulkan.h>
+#include <cassert>
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <vulkan/vulkan.h>
 
-int main() {
+VkInstance instance{VK_NULL_HANDLE};
+
+static inline void chk(VkResult result) {
+  if (result != VK_SUCCESS) {
+    std::cerr << "Vulkan call returned an error (" << result << ")\n";
+    exit(result);
+  }
+}
+
+static inline void chk(bool result) {
+  if (!result) {
+    std::cerr << "Call returned an error\n";
+    exit(result);
+  }
+}
+
+int main(int argc, char *argv[]) {
   VkApplicationInfo appInfo = {.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
                                .pApplicationName = "Mine",
                                .apiVersion = VK_API_VERSION_1_3};
@@ -11,5 +31,53 @@ int main() {
   char const *const *instanceExtensions{
       SDL_Vulkan_GetInstanceExtensions(&instanceExtensionCount)};
 
+  VkInstanceCreateInfo instanceCreateInfo = {
+      .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+      .pApplicationInfo = &appInfo,
+      .enabledExtensionCount = instanceExtensionCount,
+      .ppEnabledExtensionNames = instanceExtensions};
+  chk(vkCreateInstance(&instanceCreateInfo, nullptr, &instance));
+
+  uint32_t deviceCount{0};
+  chk(vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr));
+  std::vector<VkPhysicalDevice> devices(deviceCount);
+  chk(vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data()));
+
+  uint32_t deviceIndex{0};
+  if (argc > 1) {
+    deviceIndex = std::stoi(argv[1]);
+    assert(deviceIndex < deviceCount);
+  }
+
+  VkPhysicalDeviceProperties2 deviceProperties = {
+      .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2};
+  vkGetPhysicalDeviceProperties2(devices[deviceIndex], &deviceProperties);
+  std::cout << "Device: " << deviceProperties.properties.deviceName
+            << std::endl;
+
+  uint32_t queueFamilyCount{0};
+  vkGetPhysicalDeviceQueueFamilyProperties(devices[deviceIndex],
+                                           &queueFamilyCount, nullptr);
+  std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+  vkGetPhysicalDeviceQueueFamilyProperties(
+      devices[deviceIndex], &queueFamilyCount, queueFamilies.data());
+
+  uint32_t queueFamily{0};
+  for (uint32_t i = 0; i < queueFamilies.size(); i++) {
+    if (queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+      queueFamily = i;
+      break;
+    }
+  }
+  chk(SDL_Vulkan_GetPresentationSupport(instance, devices[deviceIndex],
+                                        queueFamily));
+
+  const float qfpriorities{1.0f};
+  VkDeviceQueueCreateInfo queueCI{
+      .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+      .queueFamilyIndex = queueFamily,
+      .queueCount = 1,
+      .pQueuePriorities = &qfpriorities,
+  };
   return 0;
 }
